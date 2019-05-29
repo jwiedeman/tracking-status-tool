5e324eb634c6a2491a11bf4fc7d33268e0a27385
Joshua Wiedeman <vielodesign@gmail.com>
2019-05-28 21:36:06 -0700

    changes

--- index.js
+++ index.js
@@ -18,35 +18,37 @@
 */
 
 // #justnodethings
+console.clear()
 var fs = require("fs");
 const puppeteer = require('puppeteer');
 var sitemaps = require('sitemap-stream-parser'); 
 const URL = require('url');
 process.setMaxListeners(Infinity); // y e e t
 
-let tick =0
-const GOOGLEANALYTICS = 'Google Analytics';
-const FACEBOOK = 'Facebook Ads';
-const BING = 'Bing Ads' 
-const GOOGLEADS = 'Google Ads';
+let tick = 0
+const GOOGLEANALYTICS = 'googleAnalytics';
+const FACEBOOK = 'facebook';
+const BING = 'bing' 
+const GOOGLEADS = 'googleAds';
 let reportJson = {};
 let promiseArr = [];
 let targetSite ='http://whostracking.me/sitemap.xml' // Parent target, this will be the base URL to work off of
-let currentTarget =''; //current url being tried
-const exampleSites = [];
+let currentTarget = ''; //current url being tried
+const sitePage = [];
 var urls = ['http://whostracking.me/sitemap.xml'];
 
+const site = [];
 
 
+const checkPage = async PAGE => {
+    currentTarget = PAGE // kinda backwards way of making PAGE acessible 
+    const _page = { // instance obj
+        'googleAnalytics': {},
+        'facebook': {},
+        'bing': {},
+        'googleAds': {} 
+    };
 
-const checkPage = async site => {
-    currentTarget = site // kinda backwards way of making site acessible 
-	const trackingInformation = {
-		'Google Analytics': {},
-		'Facebook Ads': {},
-		'Bing Ads': {},
-		'Google Ads': {} /
-	};
 	
 	const browser = await puppeteer.launch({ headless: true });
 	const page = await browser.newPage();
@@ -59,16 +61,18 @@ const checkPage = async site => {
 		if (url.indexOf('google-analytics.com') > -1) { // if analytics detected
 			const parsedUrl = URL.parse(url, true); // if the url == the url.indexof true
 			if (typeof parsedUrl.query.tid != 'undefined' && typeof parsedUrl.query.t != 'undefined') {
-				//console.log(`## GOOGLE ANALYTICS: ${site} - ${parsedUrl.query.t.toUpperCase()}`);
-				storeData(GOOGLEANALYTICS, { id: parsedUrl.query.tid, hitType: parsedUrl.query.t ,payload:parsedUrl});
+				console.log(`## GOOGLE ANALYTICS: ${parsedUrl.query.tid} - ${parsedUrl.query.t.toUpperCase()}`);
+                //storeData(GOOGLEANALYTICS, { id: parsedUrl.query.tid, hitType: parsedUrl.query.t ,payload:parsedUrl});
+                storeData(GOOGLEANALYTICS, { 'id': parsedUrl.query.tid, 'hitType': 'Pageview','payload':parsedUrl })
 			}
 
 		// facebook pixel 
 		} else if (url.indexOf('facebook.com/tr/') > -1) {
 			const parsedUrl = URL.parse(url, true);
 			if (typeof parsedUrl.query.id != 'undefined' && typeof parsedUrl.query.id != 'undefined') {
-				//console.log(`## FACEBOOK PIXEL: ${site} - ${parsedUrl.query.ev.toUpperCase()}`);
-				storeData(FACEBOOK, { id: parsedUrl.query.id, hitType: parsedUrl.query.ev ,payload:parsedUrl});
+				//console.log(`## FACEBOOK PIXEL: ${PAGE} - ${parsedUrl.query.ev.toUpperCase()}`);
+                //storeData(FACEBOOK, { id: parsedUrl.query.id, hitType: parsedUrl.query.ev ,payload:parsedUrl});
+                storeData(FACEBOOK, { 'id': parsedUrl.query.id, 'hitType': 'Pageview','payload':parsedUrl })
 			}
 
 		// google ads
@@ -76,26 +80,52 @@ const checkPage = async site => {
             let parsedUrl = URL.parse(url, true);
             let conversionId = parsedUrl.pathname.split('/')[3];
             // Check if the URL contained query string values for the conversion ID
-            //console.dir(`## GOOGLE ADS : ${site} - ${conversionId}`);
-            storeData(GOOGLEADS, { id: conversionId ,payload:parsedUrl});
+            //console.dir(`## GOOGLE ADS : ${PAGE} - ${conversionId}`);
+            //storeData(GOOGLEADS, { id: conversionId ,payload:parsedUrl});
+            storeData(GOOGLEADS, { 'id': 'conversionId', 'hitType':'Base Code Load','payload':parsedUrl })
 
         // microsoft ads
           } else if (url.indexOf('bat.bing.com/action') > -1) {
 			const parsedUrl = URL.parse(url, true);
 			if (parsedUrl.protocol == 'https:' &&typeof parsedUrl.query.ti != 'undefined' && typeof parsedUrl.query.evt != 'undefined') {
-				//console.dir(`## BING UET: ${site} - ${parsedUrl.query.evt}`);
-				storeData(BING, { id: parsedUrl.query.ti, hitType: parsedUrl.query.evt,payload:parsedUrl });
+				//console.dir(`## BING UET: ${PAGE} - ${parsedUrl.query.evt}`);
+                //storeData(BING, { id: parsedUrl.query.ti, hitType: parsedUrl.query.evt,payload:parsedUrl });
+                storeData(BING, { 'id': parsedUrl.query.ti, 'hitType':'Pageview','payload':parsedUrl })
 			}
-		}
+        }
+       
 	});
 	
 
-	await page.goto(site); // get the site
+	await page.goto(PAGE); // get the PAGE
 	await browser.close(); 
 	
-	reportJson[site] = trackingInformation; //assign reportjson for this crawl instance to the info setup on function load
-	//i.e reportJson.site now contains the empty platforms from tracking information, which will be filled as the page is crawled
-	//EX: storeData(BING, { id: parsedUrl.query.ti, hitType: parsedUrl.query.evt });
+	reportJson[PAGE] =_page; //assign reportjson for this crawl instance to the info setup on function load
+	//i.e reportJson.PAGE now contains the empty platforms from tracking information, which will be filled as the page is crawled
+    //EX: storeData(BING, { id: parsedUrl.query.ti, hitType: parsedUrl.query.evt });
+    
+
+
+    // site > page > platforms > values > payload
+    // arr > obj  > obj > obj > obj
+    
+
+    
+    
+    function storeData(platform, dataObj) {
+    let { id, hitType ,payload} = dataObj; // assign references to the data parameter
+    
+    if(typeof id != undefined && typeof hitType != undefined){ // check if incoming data exists, proceed if true
+     let data = _page[platform]
+	 data[id] = (typeof id !=undefined) ?  {'id':id} : {}
+	 data[id][hitType] = (typeof hitType !=undefined) ?  hitType : {}
+     data[id][hitType][payload] = ( payload !=undefined) ? payload : {}
+    
+    }
+    }
+    
+    
+    /** Deprecieated code, TODO Delete
 	function storeData(platform, dataObj) { //intake as params
 		let { id, hitType ,payload} = dataObj; // set structure for incoming 2 param object, now acessible as normal id, hittype
 		let info = trackingInformation[platform];
@@ -112,27 +142,27 @@ const checkPage = async site => {
 				? payload //assign info ID 
 				: {};
 		} 
-	}
+	}*/
 };
 
 
 
 
 // generate the sitemap
-sitemaps.parseSitemaps(urls, function(url) { exampleSites.push(url); }, function(err, sitemaps) {
-    exampleSites.forEach(element => {
+sitemaps.parseSitemaps(urls, function(url) { sitePage.push(url); }, function(err, sitemaps) {
+    sitePage.forEach(element => {
         console.log('# Sitemap Crawler found - ',element)
     });
-    onSitemapComplete() // calling the crawl after this to be sure the sitemap is generated before we c r a w l 
+    onSitemapComplete() // calling the crawl after this to be sure the sitemap is generated before we  c r a w l 
 });
 
 // go through arr of sites and run checksites against each
 function onSitemapComplete(){
-    exampleSites.forEach(site => {
+    sitePage.forEach(page => {
 
-		reportJson[site] = {}; // make anon instance of object for parent site
+		reportJson[page] = {}; // make anon instance of object for parent site
 		console.log('Testing  ', currentTarget);
-		promiseArr.push(checkPage(site)); // get site && push to arr
+		promiseArr.push(checkPage(page)); // get site && push to arr
 		
 	});
 	Promise.all(promiseArr).then(() => {
@@ -140,7 +170,10 @@ function onSitemapComplete(){
 		found()
 		fs.writeFile("./json.json", JSON.stringify(reportJson), (err) => {
 			console.log("File has been created");
-		});
+        });
+        
+
+
 	});
 
 }
@@ -148,108 +181,15 @@ function onSitemapComplete(){
 
 
 function found(){
-	exampleSites.forEach(site => {
-console.log('#### Results for ',site )
+	sitePage.forEach(page => {
+       // console.log('#### Results for ',site )
 		//console.log('# Google Ads',reportJson[site]['Google Ads'])
 		//console.log('# Google Analytics',reportJson[site]['Google Analytics'])
 		//console.log('# Facebook',reportJson[site]['Facebook'])
 		//console.log('# Bing',reportJson[site]['Bing'])
-		
+        //console.log('# Google Analytics',reportJson[site])
+        //console.log('### ' + reportJson[page]['googleAnalytics'] + ' - ' + page)
 	});
 }
 
-
-
-
-/**
- *   "https://whostracking.me/": {
-        "Google Analytics": {
-            "UA-116843097-1": {
-                "pageview": 1,
-                "payload": {
-                    "protocol": "https:",
-                    "slashes": true,
-                    "auth": null,
-                    "host": "www.google-analytics.com",
-                    "port": null,
-                    "hostname": "www.google-analytics.com",
-                    "hash": null,
-                    "search": "?v=1&_v=j75&a=322479791&t=pageview&_s=1&dl=https%3A%2F%2Fwhostracking.me%2F&ul=en-us&de=UTF-8&dt=Whos%20Tracking%20me%3F%20%7C%20whostrackingme&sd=24-bit&sr=800x600&vp=800x600&je=0&_u=IAhAAUAB~&jid=532312629&gjid=1763592671&cid=424356549.1559071000&tid=UA-116843097-1&_gid=211327578.1559071001&_r=1&gtm=2ou5f2&z=2120798624",
-                    "query": {
-                        "v": "1",
-                        "_v": "j75",
-                        "a": "322479791",
-                        "t": "pageview",
-                        "_s": "1",
-                        "dl": "https://whostracking.me/",
-                        "ul": "en-us",
-                        "de": "UTF-8",
-                        "dt": "Whos Tracking me? | whostrackingme",
-                        "sd": "24-bit",
-                        "sr": "800x600",
-                        "vp": "800x600",
-                        "je": "0",
-                        "_u": "IAhAAUAB~",
-                        "jid": "532312629",
-                        "gjid": "1763592671",
-                        "cid": "424356549.1559071000",
-                        "tid": "UA-116843097-1",
-                        "_gid": "211327578.1559071001",
-                        "_r": "1",
-                        "gtm": "2ou5f2",
-                        "z": "2120798624"
-                    },
-                    "pathname": "/r/collect",
-                    "path": "/r/collect?v=1&_v=j75&a=322479791&t=pageview&_s=1&dl=https%3A%2F%2Fwhostracking.me%2F&ul=en-us&de=UTF-8&dt=Whos%20Tracking%20me%3F%20%7C%20whostrackingme&sd=24-bit&sr=800x600&vp=800x600&je=0&_u=IAhAAUAB~&jid=532312629&gjid=1763592671&cid=424356549.1559071000&tid=UA-116843097-1&_gid=211327578.1559071001&_r=1&gtm=2ou5f2&z=2120798624",
-                    "href": "https://www.google-analytics.com/r/collect?v=1&_v=j75&a=322479791&t=pageview&_s=1&dl=https%3A%2F%2Fwhostracking.me%2F&ul=en-us&de=UTF-8&dt=Whos%20Tracking%20me%3F%20%7C%20whostrackingme&sd=24-bit&sr=800x600&vp=800x600&je=0&_u=IAhAAUAB~&jid=532312629&gjid=1763592671&cid=424356549.1559071000&tid=UA-116843097-1&_gid=211327578.1559071001&_r=1&gtm=2ou5f2&z=2120798624"
-                }
-            }
-        },
-        "Facebook Ads": {
-            "1968766133437314": {
-                "PageView": 1,
-                "payload": {
-                    "protocol": "https:",
-                    "slashes": true,
-                    "auth": null,
-                    "host": "www.facebook.com",
-                    "port": null,
-                    "hostname": "www.facebook.com",
-                    "hash": null,
-                    "search": "?id=1968766133437314&ev=GeneralEvent&dl=https%3A%2F%2Fwhostracking.me%2F&rl=&if=false&ts=1559071007320&cd[post_type]=page&cd[post_id]=15&cd[content_name]=Whos%20Tracking%20me%3F&cd[domain]=whostracking.me&cd[user_roles]=guest&cd[plugin]=PixelYourSite&sw=800&sh=600&v=2.8.47&r=stable&a=dvpixelyoursite&ec=1&o=158&fbp=fb.1.1559071007285.910557468&it=1559071006211&coo=false&rqm=GET",
-                    "query": {
-                        "id": "1968766133437314",
-                        "ev": "GeneralEvent",
-                        "dl": "https://whostracking.me/",
-                        "rl": "",
-                        "if": "false",
-                        "ts": "1559071007320",
-                        "cd[post_type]": "page",
-                        "cd[post_id]": "15",
-                        "cd[content_name]": "Whos Tracking me?",
-                        "cd[domain]": "whostracking.me",
-                        "cd[user_roles]": "guest",
-                        "cd[plugin]": "PixelYourSite",
-                        "sw": "800",
-                        "sh": "600",
-                        "v": "2.8.47",
-                        "r": "stable",
-                        "a": "dvpixelyoursite",
-                        "ec": "1",
-                        "o": "158",
-                        "fbp": "fb.1.1559071007285.910557468",
-                        "it": "1559071006211",
-                        "coo": "false",
-                        "rqm": "GET"
-                    },
-                    "pathname": "/tr/",
-                    "path": "/tr/?id=1968766133437314&ev=GeneralEvent&dl=https%3A%2F%2Fwhostracking.me%2F&rl=&if=false&ts=1559071007320&cd[post_type]=page&cd[post_id]=15&cd[content_name]=Whos%20Tracking%20me%3F&cd[domain]=whostracking.me&cd[user_roles]=guest&cd[plugin]=PixelYourSite&sw=800&sh=600&v=2.8.47&r=stable&a=dvpixelyoursite&ec=1&o=158&fbp=fb.1.1559071007285.910557468&it=1559071006211&coo=false&rqm=GET",
-                    "href": "https://www.facebook.com/tr/?id=1968766133437314&ev=GeneralEvent&dl=https%3A%2F%2Fwhostracking.me%2F&rl=&if=false&ts=1559071007320&cd[post_type]=page&cd[post_id]=15&cd[content_name]=Whos%20Tracking%20me%3F&cd[domain]=whostracking.me&cd[user_roles]=guest&cd[plugin]=PixelYourSite&sw=800&sh=600&v=2.8.47&r=stable&a=dvpixelyoursite&ec=1&o=158&fbp=fb.1.1559071007285.910557468&it=1559071006211&coo=false&rqm=GET"
-                },
-                "GeneralEvent": 1
-            }
-        },
-        "Bing Ads": {},
-        "Google Ads": ["r20190522"]
-    },
- */
+// console.log('# Google Analytics',reportJson[site]['googleAnalytics'])
